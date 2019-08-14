debcdca7449ee453793d10f81a9527e196b3c1f5
frostnovus <psolante@deakin.edu.au>
2019-08-14 21:36:38 +1000

    initial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Uploads></Uploads>
+  </div>
+</template>
+
+<script>
+import Uploads from "./components/Uploads.vue";
+import Header from "./components/layout/Header";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Uploads
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0, 0, 0, 0;
+}
+</style>
---
+++ src/components/Uploads.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Hello</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Uploads"
+};
+</script>
+
+<style scoped>
+.container {
+  display: grid | inline-grid;
+}
+</style>
