db5c40749ad1948279364d5ca70fd5285712276c
Paolo Nicholo Solante <nicksolante1@gmail.com>
2019-08-16 16:02:04 +1000

    Data get wit lodash
    Now able to take data from the csv

--- src/components/Uploads.vue
+++ src/components/Uploads.vue
@@ -18,12 +18,14 @@
 </template>
 <script>
 import Papa from 'papaparse';
+import get from 'lodash/get';
 
 export default {
   data () {
     return {
       file: null,
-      results: null
+      results: null,
+      header: true
     }
   },
   watch: {
@@ -32,8 +34,29 @@ export default {
         Papa.parse(newVal, {
           header: true,
           complete: (results, file) => {
+            Object.size = function (obj) {
+              var size = 0
+              var key
+              for (key in obj) {
+                if (obj.hasOwnProperty(key)) size++
+              }
+              return size
+            };
             this.results = results.meta.fields
-            console.log(results)
+
+            console.log(this.results)
+            const arrayResults = this.results
+            const arrayResultsData = results.data
+
+            console.log(arrayResults[0])
+            console.log(arrayResultsData)
+            const arrayResultsThing = arrayResultsData.map(d => {
+              return get(d, arrayResults[0], '-')
+            })
+            // const arrayResultsThing = arrayResultsData.map(d => {
+            //   return result => result[arrayResults[0]];
+            // });
+            console.log(arrayResultsThing)
           }
         })
       }
