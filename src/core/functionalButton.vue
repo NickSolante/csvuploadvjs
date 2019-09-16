081316c386a193b29c50d30221b5a48f17b6e021
frostnovus <psolante@deakin.edu.au>
2019-09-17 06:25:00 +1000

    pus

--- src/core/functionalButton.vue
+++ src/core/functionalButton.vue
@@ -68,34 +68,37 @@ export default {
     }
   },
   methods: {
-    ZoneFunc(number) {
+    ZoneFunc (number) {
       switch (number) {
         case 1:
           this.carrierTimes()
           break
         case 2:
+          this.GetUUID()
+          break
       }
     },
-    carrierTimes() {
+    carrierTimes () {
       console.log('hello')
     },
-    GetUUID() {
-
-      var seed = Date.now();
-      if (window.performance && typeof window.performance.now === "function") {
-        seed += performance.now();
+    GetUUID () {
+      var seed = Date.now()
+      if (window.performance && typeof window.performance.now === 'function') {
+        seed += performance.now()
       }
 
       var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = (seed + Math.random() * 16) % 16 | 0;
-        seed = Math.floor(seed / 16);
+        var r = (seed + Math.random() * 16) % 16 | 0
+        seed = Math.floor(seed / 16)
 
-        return (c === 'x' ? r : r & (0x3 | 0x8)).toString(16);
-      });
+        return (c === 'x' ? r : r & (0x3 | 0x8)).toString(16)
+      })
 
-      return uuid;
+      return uuid
     }
+
   }
+}
 </script>
 
 <style>
